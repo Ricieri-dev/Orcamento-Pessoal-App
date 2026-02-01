cae6a0728114b1bbfa59d2e83ad68831934061a0
Filipe  Ricieri <ricieri.filipe@hotmail.com>
2026-02-01 00:48:03 -0300

    consulta de despesas parte 1

--- app.js
+++ app.js
@@ -36,7 +36,30 @@ class Bd{
 
         localStorage.setItem(id.toString(), JSON.stringify(d))
         localStorage.setItem('id', id.toString())
-    }   
+    }
+    
+    recuperarTodosRegistros(){
+
+        //array de despesas 
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas cadastradas localStorage
+        for (let i= 1; i <= id; i++){
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+            //verificar se existe a possibilidade de haver índices que foram pulados/removidos
+            //nestes casos, nós vamos pular esses índices
+            if(despesa === null){
+                continue
+            }
+            despesas.push(despesa)
+        
+        }
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -94,8 +117,12 @@ function cadastrarDespesa(){
 
         $('#modalRegistraDespesa').modal('show')
     }
-    
+}
 
+function carregaListaDespesas(){
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+    console.log(despesas)
 
 }
 
