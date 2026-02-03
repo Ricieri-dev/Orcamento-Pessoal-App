3a7c3692992298430e4e80e3a6525b7c404b8659
Filipe Ricieri <ricieri.filipe@hotmail.com>
2026-02-03 17:09:15 -0300

    filtrando despesas parte 1

--- app.js
+++ app.js
@@ -60,6 +60,11 @@ class Bd{
         }
         return despesas
     }
+
+    pesquisar(despesa){
+        console.log(despesa)
+    }
+
 }
 
 let bd = new Bd()
@@ -86,6 +91,8 @@ function cadastrarDespesa(){
     if (despesa.validarDados()){
         bd.gravar(despesa)
 
+        
+
         let titulo = document.getElementById('exampleModalLabel')
         let corpo = document.getElementById('modalBody')
         let botao = document.getElementById('botaoModal')
@@ -100,6 +107,12 @@ function cadastrarDespesa(){
         botao.classList.add('btn-success')
 
         $('#modalRegistraDespesa').modal('show')
+        ano.value  = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = '' 
+        valor.value = ''
 
     }else{
         let titulo = document.getElementById('exampleModalLabel')
@@ -126,20 +139,12 @@ function carregaListaDespesas(){
     //selecionando o elemento tbody da tabela 
     let listaDespesas = document.getElementById('listaDespesas')
 
-    /*
-    <tr>
-        <td>02/02/2025</td>
-        <td>Alimentação</td>
-        <td>Compra do Mês</td>
-        <td>1500.00</td>
-    </tr>
-    */
 
     //percorrer o array despesas, listando cada despesa de forma dinâmica
 
     despesas.forEach(function(d){
 
-        console.log(d)
+        
         
         //criando a linha (tr)
         let linha = listaDespesas.insertRow()
@@ -168,4 +173,19 @@ function carregaListaDespesas(){
 
     })
 }
+
+function pesquisarDespesa(){
+    let ano = document.getElementById('ano').value;
+    let mes = document.getElementById('mes').value;
+    let dia = document.getElementById('dia').value;
+    let tipo = document.getElementById('tipo').value;
+    let descricao = document.getElementById('descricao').value;
+    let valor = document.getElementById('valor').value
+
+    despesa = new Despesa(ano,mes,dia,tipo,descricao,valor)
+
+    bd.pesquisar(despesa)
+    
+}
+
 
